1ff2dd815ac5e4145464fd84c417e8887121742f
Andrew A. Cashner <andrewacashner@gmail.com>
2016-06-29 17:02:00 -0700

    Al establo corrections; testing pagebreak in reledmac stanzas

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -i *.aux *.1 *.1R *.2 *.2R *.3 *.3R *.4 *.4R *.5 *.5R *.bbl *.bcf *.blg *.log *.run.xml *.toc
+rm -i aux/* *.aux *.1 *.1R *.2 *.2R *.3 *.3R *.4 *.4R *.5 *.5R *.bbl *.bcf *.blg *.log *.run.xml *.toc
