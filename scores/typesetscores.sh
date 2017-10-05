8a341e572f2d62eed6a24ecd25ef40d4c8f355be
Andrew A. Cashner <andrewacashner@gmail.com>
2017-10-05 13:49:06 -0400

    transposing scores in make script

--- scores/typesetscores.sh
+++ scores/typesetscores.sh
@@ -6,3 +6,12 @@ do
   cd "$HOME/Documents/vcscores/scores/$scorename"
   lilypond -I ~/ly score
 done
+
+mapfile -t scoredirectories < \
+"$HOME/Documents/vcscores/scores/scorelist-transposed.txt"
+for scorename in "${scoredirectories[@]}";
+do
+  echo -e "\nCompiling transposed $scorename ..."
+  cd "$HOME/Documents/vcscores/scores/$scorename"
+  lilypond -I ~/ly -dtranspose -o score-transposed score
+done
